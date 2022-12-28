37e8da61807adb661cbcf603fc86e3bb66574374
Alex Kir <alexkir800@gmail.com>
2022-12-29 01:44:21 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import ru.netology.sqr.SQRService;
+
+public class Main {
+    public static void main(String[] args) {
+        SQRService service = new SQRService();
+        int min = 200;
+        int max = 300;
+        int count = service.calculate(min, max);
+
+        System.out.println(count);
+
+    }
+
+}
